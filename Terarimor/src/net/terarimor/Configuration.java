1c0202d1b9b866460ab997e02c3bfe60d6078433
KingOfL33t <KingOfL33tGaming@gmail.com>
2013-11-21 10:17:28 -0600

    Added the configuration and some basic values

---
+++ Terarimor/src/net/terarimor/Configuration.java
@@ -0,0 +1,10 @@
+package net.terarimor;
+
+//The settings and configurations for various parts of the program
+public class Configuration {
+	public int window_height = 400;
+	public int window_width = 600;
+	public String window_title = "Terarimor!";
+	
+	
+}
