5efbe78a718c82dde37955efec887de9d78cc62b
KingOfL33t <KingOfL33tGaming@gmail.com>
2013-11-20 21:34:14 -0600

    Event handling logic update

---
+++ Terarimor/src/net/terarimor/event/EventPriority.java
@@ -0,0 +1,18 @@
+package net.terarimor.event;
+
+public enum EventPriority {
+	LOWEST(0),
+	LOW(1),
+	NORMAL(2),
+	HIGH(3),
+	HIGHEST(4),
+	MONITOR(5);
+	
+	private final int priority;
+	private EventPriority(int priority){
+		this.priority = priority;
+	}
+	private int getPriority(){
+		return priority;
+	}
+}
---
+++ Terarimor/src/net/terarimor/event/Listener.java
@@ -0,0 +1,3 @@
+package net.terarimor.event;
+
+public interface Listener {}
