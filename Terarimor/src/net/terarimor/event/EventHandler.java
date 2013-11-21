6de53b953cacb8e116f2208029f970ebc35c35d3
KingOfL33t <KingOfL33tGaming@gmail.com>
2013-11-20 21:33:43 -0600

    Started working on basic Handler

--- Terarimor/src/net/terarimor/event/EventHandler.java
+++ Terarimor/src/net/terarimor/event/EventHandler.java
@@ -1,5 +1,11 @@
 package net.terarimor.event;
 
-public class EventHandler {
-
+public interface EventHandler {
+	/**
+	 * The priority of the event
+	 * (LOWEST, LOW, NORMAL, HIGH, HIGHEST, MONITOR)
+	 */
+	EventPriority priority() default EventPriority.NORMAL;
+	
+	boolean ignoreCancelled() default false;
 }
