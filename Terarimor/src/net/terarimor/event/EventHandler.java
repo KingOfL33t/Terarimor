8bd51e2927607e3ac2adb5610893ff6d376aeb2a
KingOfL33t <KingOfL33tGaming@gmail.com>
2013-11-20 22:06:51 -0600

    Made a basic interface annotation

--- Terarimor/src/net/terarimor/event/EventHandler.java
+++ Terarimor/src/net/terarimor/event/EventHandler.java
@@ -1,11 +1,18 @@
 package net.terarimor.event;
 
-public interface EventHandler {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/** An annotation to mark methods as being event handler methods */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface EventHandler {
 	/**
-	 * The priority of the event
-	 * (LOWEST, LOW, NORMAL, HIGH, HIGHEST, MONITOR)
+	 * The priority of the event (LOWEST, LOW, NORMAL, HIGH, HIGHEST, MONITOR)
 	 */
 	EventPriority priority() default EventPriority.NORMAL;
-	
+
 	boolean ignoreCancelled() default false;
 }
