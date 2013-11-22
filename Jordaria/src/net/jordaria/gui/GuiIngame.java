e9753adf6e13a9300557697eba07d8c296a6cb5c
KingOfL33t <KingOfL33tGaming@gmail.com>
2013-11-21 18:25:26 -0600

    Removed imports

--- Jordaria/src/net/jordaria/gui/GuiIngame.java
+++ Jordaria/src/net/jordaria/gui/GuiIngame.java
@@ -3,9 +3,12 @@ package net.jordaria.gui;
 import net.jordaria.Jordaria;
 
 public class GuiIngame extends Gui{
+	
 	private final Jordaria jordaria;
+	
 	public GuiIngame(Jordaria jd){
 		jordaria = jd;
 	}
+	
 
 }
