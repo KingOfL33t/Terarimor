74354d4912088a568b8a576c48167e858c010293
KingOfL33t <KingOfL33tGaming@gmail.com>
2013-11-21 18:52:30 -0600

    Modified configured values

--- Jordaria/src/net/jordaria/Configuration.java
+++ Jordaria/src/net/jordaria/Configuration.java
@@ -5,9 +5,7 @@ public class Configuration {
 	public static int window_height = 480;
 	public static int window_width = 640;
 	public static String window_title = "Jordaria!";
-	public static int default_display_height = 480;
-	public static int default_display_width = 640;
-	public static int default_display_bitsPerPixel = 32;
+	public static int display_bitsPerPixel = 32;
 	
 	public int getWindow_height(){
 		return window_height;
@@ -18,14 +16,8 @@ public class Configuration {
 	public String getWindow_title(){
 		return window_title;
 	}
-	public int getDefault_display_height() {
-		return default_display_height;
-	}
-	public int getDefault_display_width() {
-		return default_display_width;
-	}
-	public int getDefault_display_bitsPerPixel() {
-		return default_display_bitsPerPixel;
+	public int getDisplay_bitsPerPixel() {
+		return display_bitsPerPixel;
 	}
 	
 }
