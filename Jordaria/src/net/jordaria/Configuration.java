bd670e39f5a136786b1b522a75748b9a0722dde8
KingOfL33t <KingOfL33tGaming@gmail.com>
2013-11-21 18:24:40 -0600

    Added more configurations

--- Jordaria/src/net/jordaria/Configuration.java
+++ Jordaria/src/net/jordaria/Configuration.java
@@ -2,9 +2,12 @@ package net.jordaria;
 
 //The settings and configurations for various parts of the program
 public class Configuration {
-	public static int window_height = 400;
-	public static  int window_width = 600;
+	public static int window_height = 480;
+	public static int window_width = 640;
 	public static String window_title = "Jordaria!";
+	public static int default_display_height = 480;
+	public static int default_display_width = 640;
+	public static int default_display_bitsPerPixel = 32;
 	
 	public int getWindow_height(){
 		return window_height;
@@ -14,6 +17,15 @@ public class Configuration {
 	}
 	public String getWindow_title(){
 		return window_title;
+	}
+	public int getDefault_display_height() {
+		return default_display_height;
+	}
+	public int getDefault_display_width() {
+		return default_display_width;
+	}
+	public int getDefault_display_bitsPerPixel() {
+		return default_display_bitsPerPixel;
 	}
 	
 }
