30b3a03da7d7c0b95c4886aaa95d55a342599312
KingOfL33t <KingOfL33tGaming@gmail.com>
2013-11-22 10:25:44 -0600

    Added more rendering code

--- Jordaria/src/net/jordaria/world/Chunk.java
+++ Jordaria/src/net/jordaria/world/Chunk.java
@@ -10,7 +10,7 @@ import net.jordaria.block.Block;
 import net.jordaria.Configuration;
 
 public class Chunk {
-	private short[][][] blocks;
+	private Block[][][] blocks;
 	public final int xPosition;
 	public final int yPosition;
 	public final int zPosition;
@@ -31,26 +31,30 @@ public class Chunk {
 	public void update(){
 
 	}
+
 	public Chunk(int xPos, int yPos, int zPos){
 		xPosition = xPos;
 		yPosition = yPos;
 		zPosition = zPos;
-		blocks = new short[Configuration.getCHUNK_SIZE()][Configuration.getCHUNK_SIZE()][Configuration.getCHUNK_SIZE()];
+		blocks = new Block[Configuration.getCHUNK_SIZE()][Configuration.getCHUNK_SIZE()][Configuration.getCHUNK_SIZE()];
 		for (int x = 0; x < Configuration.getCHUNK_SIZE(); x++){
 			for (int y = 0; y< Configuration.getCHUNK_SIZE(); y++){
 				for (int z = 0; z < Configuration.getCHUNK_SIZE(); z++){
 					if (x==0){
-						blocks[x][y][z] = Block.stone.blockID;
+						blocks[x][y][z] = Block.stone;
 					}
 					else if (x==1){
-						blocks[x][y][z] = Block.grass.blockID;
+						blocks[x][y][z] = Block.grass;
 					}
 					else{
-						blocks[x][y][z] = Block.air.blockID;
+						blocks[x][y][z] = Block.air;
 					}
 				}
 			}
 		}
+		VBOColorHandle = GL15.glGenBuffers();
+		VBOVertexHandle = GL15.glGenBuffers();
+		RebuildMesh(xPos, yPos, zPos);
 	}
 
 	public void RebuildMesh(float startX, float startY, float startZ) {
