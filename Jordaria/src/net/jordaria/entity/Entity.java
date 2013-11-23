ffcd4b5a1063da870e92b3ccc0d95c6d2bee399b
KingOfL33t <KingOfL33tGaming@gmail.com>
2013-11-23 10:44:43 -0600

    Added more information

--- Jordaria/src/net/jordaria/entity/Entity.java
+++ Jordaria/src/net/jordaria/entity/Entity.java
@@ -1,6 +1,19 @@
 package net.jordaria.entity;
 
+import net.jordaria.world.World;
+
 public abstract class Entity {
 	public int id;
-
+	public World worldObj;
+	
+	public double posX;//x position
+    public double posY;//y position
+    public double posZ;//z position
+    public float rotationYaw;
+    public float rotationPitch;
+    public double velX;//velocity in the x direction
+    public double velY;//velocity in the y direction
+    public double velZ;//velocity in the z direction
+    public float width;
+    public float height;
 }
