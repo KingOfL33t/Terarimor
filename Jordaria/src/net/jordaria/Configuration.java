543b911e9c039b1021c326e5a8da82c2378ab806
KingOfL33t <KingOfL33tGaming@gmail.com>
2013-11-23 14:36:07 -0600

    Updated description in comments

--- Jordaria/src/net/jordaria/Configuration.java
+++ Jordaria/src/net/jordaria/Configuration.java
@@ -1,6 +1,10 @@
 package net.jordaria;
 
-//The settings and configurations for various parts of the program
+/*The settings and configurations for various parts of the program
+ * This is so changes across the entire game can be made easily,
+ * such as changing the number of blocks in a chunk,
+ * without having to redo large chunks of code
+ */
 public class Configuration {
 	public static int window_height = 480;
 	public static int window_width = 640;
