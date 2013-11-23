ecb6896799ed1219651871948db381700d7a5356
KingOfL33t <KingOfL33tGaming@gmail.com>
2013-11-23 12:56:45 -0600

    Fixed comments

--- Jordaria/src/net/jordaria/event/Event.java
+++ Jordaria/src/net/jordaria/event/Event.java
@@ -27,18 +27,17 @@ public abstract class Event {
 
 	public enum Result {
 		/**
-		 * * Deny the event. Depending on the event, the action indicated by the
+		 * * Reject the event. Depending on the event, the action indicated by the
 		 * * event will either not take place or will be reverted. Some actions
-		 * * may not be denied.
+		 * * may not be rejected.
 		 */
 		REJECT, /**
-		 * * Neither deny nor allow the event. The server will proceed with
-		 * its * normal handling.
+		 * Neither Reject nor Permit the event. The server will proceed as normal
 		 */
 		DEFAULT, /**
-		 * * Allow / Force the event. The action indicated by the event
-		 * will * take place if possible, even if the server would not normally
-		 * allow * the action. Some actions may not be allowed.
+		 * * Permit/Force the event. The action indicated by the event
+		 * will take place if possible, even if the server would not normally
+		 * allow the action. Some actions may not be permitted.
 		 */
 		PERMIT;
 	}
