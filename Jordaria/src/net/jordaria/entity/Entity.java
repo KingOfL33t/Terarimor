f073d363bc87b686bb28cd1d9e45afc426823817
KingOfL33t <KingOfL33tGaming@gmail.com>
2013-11-23 21:10:15 -0600

    added setsize method

--- Jordaria/src/net/jordaria/entity/Entity.java
+++ Jordaria/src/net/jordaria/entity/Entity.java
@@ -10,43 +10,49 @@ public abstract class Entity {
 	private static int nextEntityID;
 	private UUID entityUniqueID;
 	public World worldObj;
-	
+
 	public Random rand;
-	
+
 	public double posX;//x position
-    public double posY;//y position
-    public double posZ;//z position
-    public float rotationYaw;
-    public float rotationPitch;
-    public double velX;//velocity in the x direction
-    public double velY;//velocity in the y direction
-    public double velZ;//velocity in the z direction
-    public float width;
-    public float height;
+	public double posY;//y position
+	public double posZ;//z position
+	public float rotationYaw;
+	public float rotationPitch;
+	public double velX;//velocity in the x direction
+	public double velY;//velocity in the y direction
+	public double velZ;//velocity in the z direction
+	public float width;
+	public float height;
+	public float yOffset;
 
-    
-    public boolean addedToChunk;//set to true when it has been added to its current chunk
-    public int chunkCoordX;
-    public int chunkCoordY;
-    public int chunkCoordZ;
-    
-    public Entity(World par1World)
-    {
-        this.id = nextEntityID++;
-        this.width = 0.6F;
-        this.height = 1.8F;
-        this.rand = new Random();
-        this.entityUniqueID = UUID.randomUUID();
-        this.worldObj = par1World;
-        this.setPosition(0.0D, 0.0D, 0.0D);
-        this.entityInit();
-    }
 
-    protected abstract void entityInit();//inherited for subclass initiation
-    
+	public boolean addedToChunk;//set to true when it has been added to its current chunk
+	public int chunkCoordX;
+	public int chunkCoordY;
+	public int chunkCoordZ;
+
+	public Entity(World par1World)
+	{
+		this.id = nextEntityID++;
+		this.width = 0.6F;
+		this.height = 1.8F;
+		this.rand = new Random();
+		this.entityUniqueID = UUID.randomUUID();
+		this.worldObj = par1World;
+		this.setPosition(0.0D, 0.0D, 0.0D);
+		this.entityInit();
+	}
+
+	protected abstract void entityInit();//inherited for subclass initiation
+
 	public void setPosition(double x, double y, double z) {
 		this.posX = x;
 		this.posY = y;
 		this.posZ = z;
+	}
+
+	public void setSize(float w, float h){
+		this.width = w;
+		this.height = h;
 	}
 }
