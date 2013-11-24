973e271d4d5d46106b40db4556c8e865243d2d31
KingOfL33t <KingOfL33tGaming@gmail.com>
2013-11-23 20:00:26 -0600

    Created a class for managing chunks in the world

---
+++ Jordaria/src/net/jordaria/world/ChunkManager.java
@@ -0,0 +1,56 @@
+package net.jordaria.world;
+
+import java.util.HashMap;
+
+public class ChunkManager {
+	public HashMap<ChunkCoordinates, Chunk> chunkMap = new HashMap<ChunkCoordinates, Chunk>();
+
+	private World worldObj;
+
+	private Chunk blankChunk;
+	
+	public ChunkManager(World theWorld)
+	{
+		this.blankChunk = new Chunk(theWorld, 0, 0, 0).makeEmptyChunk();
+		this.worldObj = theWorld;
+	}
+	
+	/**
+     * loads or generates the chunk at the chunk location specified
+     */
+    public Chunk loadChunk(int xPos, int yPos, int zPos)
+    {
+        Chunk theChunk = new Chunk(this.worldObj, xPos, yPos, zPos);
+        this.chunkMap.put(new ChunkCoordinates(xPos, yPos, zPos), theChunk);
+        theChunk.isChunkLoaded = true;
+        return theChunk;
+    }
+    
+    /**
+     * Unload chunk from ChunkProviderClient's hashmap.
+     */
+    public void unloadChunk(int xPos, int yPos, int zPos)
+    {
+        Chunk theChunk = this.provideChunk(xPos, yPos, zPos);
+
+        if (!theChunk.isEmpty())
+        {
+        	theChunk.onChunkUnload();
+        }
+
+        this.chunkMap.remove(new ChunkCoordinates(xPos, yPos, zPos));
+    }
+    
+    public Chunk provideChunk(int xPos, int yPos, int zPos)
+    {
+        Chunk theChunk = (Chunk)this.chunkMap.get(new ChunkCoordinates(xPos, yPos, zPos));
+        return theChunk == null ? this.blankChunk : theChunk;
+    }
+    
+    public int getLoadedChunkCount()
+    {
+        return this.chunkMap.size();
+    }
+    
+    
+}
