71fafd37fd76b77b22315c25208f73b50ac24086
KingOfL33t <KingOfL33tGaming@gmail.com>
2013-11-25 10:22:41 -0600

    Created material class for items and blocks

---
+++ Jordaria/src/net/jordaria/material/Material.java
@@ -0,0 +1,9 @@
+package net.jordaria.material;
+
+
+public class Material {
+	public static final Material[] materialList = new Material[1024];
+	
+	public static final Material MATERIAL_STONE = new MaterialStone();
+
+}
