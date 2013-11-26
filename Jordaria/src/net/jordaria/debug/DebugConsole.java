8acc7811c178ad15bab176cdd574f896a3398333
EISD <EISD@WH-RM276-S29.eaneside.local>
2013-11-26 09:55:50 -0600

    created a console class

---
+++ Jordaria/src/net/jordaria/debug/DebugConsole.java
@@ -0,0 +1,22 @@
+package net.jordaria.debug;
+
+import java.awt.FlowLayout;
+
+import javax.swing.JPanel;
+
+public class DebugConsole extends JPanel{
+	
+	private static final long serialVersionUID = 133789912946195636L;
+	private String window_title = "Debug Console";
+	
+	public DebugConsole(){
+		super();
+		this.init();
+	}
+
+	private void init(){
+		setSize(300,200);
+		setLayout(new FlowLayout(FlowLayout.CENTER));
+		setName(window_title);
+	}
+}
