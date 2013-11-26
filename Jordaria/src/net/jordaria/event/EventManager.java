e8f49e045f35f650f95a8d3daea0e26bcd7a4aaa
KingOfL33t <KingOfL33tGaming@gmail.com>
2013-11-26 17:36:04 -0600

    Added a class for managing events and listeners

---
+++ Jordaria/src/net/jordaria/event/EventManager.java
@@ -0,0 +1,52 @@
+package net.jordaria.event;
+
+import java.lang.reflect.Method;
+
+import net.jordaria.exception.EventException;
+
+public class EventManager {
+	
+	public void registerEvent(final Class<? extends Event> type, Listener listener) throws Exception{
+		EventExecutor tmpExecutor = new EventExecutor() {
+			public void execute(Listener listener, Event event) throws EventException {
+				try {
+					if (!type.isAssignableFrom(event.getClass())) {
+						return;
+					}
+				} catch (Throwable t) {
+					throw new EventException(t);
+				}
+			}
+		};
+		getEventListeners(type).register(new EventListener(listener, tmpExecutor, EventPriority.NORMAL));
+	}
+	
+	 private HandlerList getEventListeners(Class<? extends Event> type) throws Exception {
+	        try {
+	            Method method = getRegistrationClass(type).getDeclaredMethod("getHandlerList");//get the gethandlerlist method
+	            method.setAccessible(true);//eh, security? we have a try/catch for that :P
+	            return (HandlerList) method.invoke(null);//get the handler list from the class
+	        } catch (Exception e) {
+	            throw new Exception(e.getMessage(),e.getCause());//oops
+	        }
+	    }
+
+
+	    private Class<? extends Event> getRegistrationClass(Class<? extends Event> eventClass) throws Exception {
+	        try {
+	            eventClass.getDeclaredMethod("getHandlerList");
+	            return eventClass;
+	        } catch (NoSuchMethodException e) {
+	            if (eventClass.getSuperclass() != null//super is not null
+	                    && !eventClass.getSuperclass().equals(Event.class)//and its an event subclass
+	                    && Event.class.isAssignableFrom(eventClass.getSuperclass())) {//and it should be assignable
+	                return getRegistrationClass(eventClass.getSuperclass().asSubclass(Event.class));
+	            } else {
+	                throw new Exception("Unable to find handler list for event " + eventClass.getName());//they dont exist
+	            }
+	        }
+	    }
+
+	
+	
+}
