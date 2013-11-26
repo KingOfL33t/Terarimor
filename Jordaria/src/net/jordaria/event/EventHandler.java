cb9cddb7d13d128b8b5aeeb4d9fa477b790f3aa4
EISD <EISD@WH-RM276-S29.eaneside.local>
2013-11-26 09:50:26 -0600

    removed ignore cancel

--- Jordaria/src/net/jordaria/event/EventHandler.java
+++ Jordaria/src/net/jordaria/event/EventHandler.java
@@ -13,6 +13,4 @@ public @interface EventHandler {
 	 * The priority of the event (LOWEST, LOW, NORMAL, HIGH, HIGHEST, MONITOR)
 	 */
 	EventPriority priority() default EventPriority.NORMAL;
-
-	boolean ignoreCancelled() default false;
 }
