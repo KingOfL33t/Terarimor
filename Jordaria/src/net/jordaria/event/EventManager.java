2cfa6defb717675729fe5666e582f2c82933f7f4
KingOfL33t <KingOfL33tGaming@gmail.com>
2013-11-26 19:31:30 -0600

    worked on adding listeners

--- Jordaria/src/net/jordaria/event/EventManager.java
+++ Jordaria/src/net/jordaria/event/EventManager.java
@@ -5,7 +5,7 @@ import java.lang.reflect.Method;
 import net.jordaria.exception.EventException;
 
 public class EventManager {
-	
+
 	public void registerEvent(final Class<? extends Event> type, Listener listener) throws Exception{
 		EventExecutor tmpExecutor = new EventExecutor() {
 			public void execute(Listener listener, Event event) throws EventException {
@@ -20,33 +20,32 @@ public class EventManager {
 		};
 		getEventListeners(type).register(new EventListener(listener, tmpExecutor, EventPriority.NORMAL));
 	}
-	
-	 private HandlerList getEventListeners(Class<? extends Event> type) throws Exception {
-	        try {
-	            Method method = getRegistrationClass(type).getDeclaredMethod("getHandlerList");//get the gethandlerlist method
-	            method.setAccessible(true);//eh, security? we have a try/catch for that :P
-	            return (HandlerList) method.invoke(null);//get the handler list from the class
-	        } catch (Exception e) {
-	            throw new Exception(e.getMessage(),e.getCause());//oops
-	        }
-	    }
+
+	private HandlerList getEventListeners(Class<? extends Event> type) throws Exception {
+		try {
+			Method method = getRegistrationClass(type).getDeclaredMethod("getHandlerList");//get the gethandlerlist method
+			method.setAccessible(true);//eh, security? we have a try/catch for that :P
+			return (HandlerList) method.invoke(null);//get the handler list from the class
+		} catch (Exception e) {
+			throw new Exception(e.getMessage(),e.getCause());//oops
+		}
+	}
 
 
-	    private Class<? extends Event> getRegistrationClass(Class<? extends Event> eventClass) throws Exception {
-	        try {
-	            eventClass.getDeclaredMethod("getHandlerList");
-	            return eventClass;
-	        } catch (NoSuchMethodException e) {
-	            if (eventClass.getSuperclass() != null//super is not null
-	                    && !eventClass.getSuperclass().equals(Event.class)//and its an event subclass
-	                    && Event.class.isAssignableFrom(eventClass.getSuperclass())) {//and it should be assignable
-	                return getRegistrationClass(eventClass.getSuperclass().asSubclass(Event.class));
-	            } else {
-	                throw new Exception("Unable to find handler list for event " + eventClass.getName());//they dont exist
-	            }
-	        }
-	    }
+	private Class<? extends Event> getRegistrationClass(Class<? extends Event> eventClass) throws Exception {
+		try {
+			eventClass.getDeclaredMethod("getHandlerList");
+			return eventClass;
+		} catch (NoSuchMethodException e) {
+			if (eventClass.getSuperclass() != null//super is not null
+					&& !eventClass.getSuperclass().equals(Event.class)//and its an event subclass
+					&& Event.class.isAssignableFrom(eventClass.getSuperclass())) {//and it should be assignable
+				return getRegistrationClass(eventClass.getSuperclass().asSubclass(Event.class));
+			} else {
+				throw new Exception("Unable to find handler list for event " + eventClass.getName());//they dont exist
+			}
+		}
+	}
 
-	
-	
+
 }
