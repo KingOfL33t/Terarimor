1d1e61f0d59603f97c5e4425f54c01175507aa90
KingOfL33t <KingOfL33tGaming@gmail.com>
2013-12-01 09:35:46 -0600

    Made console noneditable

--- Jordaria/src/net/jordaria/debug/DebugConsole.java
+++ Jordaria/src/net/jordaria/debug/DebugConsole.java
@@ -26,7 +26,7 @@ public class DebugConsole extends WindowAdapter implements Listener{
 		frame.setSize(width, height);
 		
 		textArea = new JTextArea();
-		textArea.setEditable(true);
+		textArea.setEditable(false);
 		textArea.setBackground(new Color(2, 3, 2));
 		textArea.setForeground(new Color(2,200,2));
 		
