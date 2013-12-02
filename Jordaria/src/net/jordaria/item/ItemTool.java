86e8eac4678872de03a450072a6633b2e085c4a7
KingOfL33t <KingOfL33tGaming@gmail.com>
2013-12-01 20:28:53 -0600

    removed durability int

--- Jordaria/src/net/jordaria/item/ItemTool.java
+++ Jordaria/src/net/jordaria/item/ItemTool.java
@@ -3,7 +3,7 @@ package net.jordaria.item;
 import net.jordaria.material.Material;
 
 public class ItemTool extends Item{
-	public int durability;
+	
 	public Material material;
 	
 	public ItemTool(){
