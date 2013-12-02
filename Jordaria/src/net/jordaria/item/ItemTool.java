dc50406998c79b04e8b45912e0bebafdf49b882d
KingOfL33t <KingOfL33tGaming@gmail.com>
2013-12-01 20:27:49 -0600

    Created a base tool class

---
+++ Jordaria/src/net/jordaria/item/ItemTool.java
@@ -0,0 +1,12 @@
+package net.jordaria.item;
+
+import net.jordaria.material.Material;
+
+public class ItemTool extends Item{
+	public int durability;
+	public Material material;
+	
+	public ItemTool(){
+		super();
+	}
+}
