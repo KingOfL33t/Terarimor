41af45fc271126e37945b88283ad36f30ddaf44c
EISD <EISD@WH-RM276-S29.eaneside.local>
2013-12-02 10:30:27 -0600

    changed to lwjgl key ints

--- Jordaria/src/net/jordaria/GameSettings.java
+++ Jordaria/src/net/jordaria/GameSettings.java
@@ -1,5 +1,7 @@
 package net.jordaria;
 
+import org.lwjgl.input.Keyboard;
+
 /*
  * Settings for use in the game. These eventually will be able to be set, saved and loaded
  */
@@ -8,12 +10,12 @@ public class GameSettings {
 	protected Jordaria jd;
 
 	//control keybinds
-	public KeyBind KEYBIND_MOVE_FORWARD = new KeyBind("key.forward", AsciiValue.W_LOWERCASE.value());
-	public KeyBind KEYBIND_MOVE_LEFT = new KeyBind("key.left", AsciiValue.A_LOWERCASE.value());
-	public KeyBind KEYBIND_MOVE_BACKWARD = new KeyBind("key.back", AsciiValue.S_LOWERCASE.value());
-	public KeyBind KEYBIND_MOVE_RIGHT = new KeyBind("key.right", AsciiValue.D_LOWERCASE.value());
-	public KeyBind KEYBIND_MOVE_JUMP = new KeyBind("key.jump", AsciiValue.SPACE.value());
-	public KeyBind KEYBIND_WIREFRAME = new KeyBind("key.wireframe", AsciiValue.F_LOWERCASE.value());
+	public KeyBind KEYBIND_MOVE_FORWARD = new KeyBind("key.forward", Keyboard.KEY_W);
+	public KeyBind KEYBIND_MOVE_LEFT = new KeyBind("key.left", Keyboard.KEY_A);
+	public KeyBind KEYBIND_MOVE_BACKWARD = new KeyBind("key.back", Keyboard.KEY_S);
+	public KeyBind KEYBIND_MOVE_RIGHT = new KeyBind("key.right", Keyboard.KEY_D);
+	public KeyBind KEYBIND_MOVE_JUMP = new KeyBind("key.jump", Keyboard.KEY_SPACE);
+	public KeyBind KEYBIND_WIREFRAME = new KeyBind("key.wireframe", Keyboard.KEY_F);
 	public KeyBind[] keyBindings;
 
 	//booleans
@@ -43,6 +45,10 @@ public class GameSettings {
 	}
 
 	public boolean isWireframe() {
+		return wireframe;
+	}
+	public boolean toggleWireframe(){//toggles wireframe and returns the new value
+		wireframe = !wireframe;
 		return wireframe;
 	}
 	public int getRenderDistance() {
