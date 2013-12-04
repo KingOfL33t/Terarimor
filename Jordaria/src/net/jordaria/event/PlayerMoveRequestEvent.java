4a8d9ee238f177ef23eb34c2cd72e95e6b0e6f9e
KingOfL33t <KingOfL33tGaming@gmail.com>
2013-12-03 21:56:46 -0600

    removed direction from player move request b/c it needs to handle angles

--- Jordaria/src/net/jordaria/event/PlayerMoveRequestEvent.java
+++ Jordaria/src/net/jordaria/event/PlayerMoveRequestEvent.java
@@ -1,17 +1,14 @@
 package net.jordaria.event;
 
-import net.jordaria.Direction;
 import net.jordaria.entity.EntityPlayer;
 
 public class PlayerMoveRequestEvent extends Event{
 	private static final HandlerList handlers = new HandlerList();
 	
 	public EntityPlayer player;
-	public Direction direction;
 	
-	public PlayerMoveRequestEvent(EntityPlayer player, Direction direction){
+	public PlayerMoveRequestEvent(EntityPlayer player){//TODO add a direction
 		this.player = player;
-		this.direction = direction;
 	}
 	
 	@Override
