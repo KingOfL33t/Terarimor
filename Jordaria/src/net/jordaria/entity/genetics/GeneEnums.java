df3a2f8853753922b34409125b6c0a961f7c44ee
KingOfL33t <KingOfL33tGaming@gmail.com>
2013-12-09 20:56:11 -0600

    Added geneenums comments

--- Jordaria/src/net/jordaria/entity/genetics/GeneEnums.java
+++ Jordaria/src/net/jordaria/entity/genetics/GeneEnums.java
@@ -1,6 +1,11 @@
 package net.jordaria.entity.genetics;
 
 public class GeneEnums {
+	/*
+	 * This specifies what position in the dna each gene resides in
+	 * The enum allows the genes to get switched around easily without
+	 * major code rehaul
+	 */
 	public enum genePosition{
 		HEALTH(0),
 		SIZE(1),
@@ -11,11 +16,15 @@ public class GeneEnums {
 			this.pos = value;
 		}
 	}
+	
+	/*
+	 * The size of the organism
+	 */
 	public enum size{
 		TINY((byte)0),
 		VERY_SMALL((byte)1),
 		SMALL((byte)2),
-		MEDIUM((byte)3),
+		NORMAL((byte)3),
 		LARGE((byte)4),
 		VERY_LARGE((byte)5),
 		HUGE((byte)6);
@@ -25,6 +34,10 @@ public class GeneEnums {
 			this.geneData = value;
 		}
 	}
+	
+	/*
+	 * Changes in the move speed of the organism
+	 */
 	public enum moveSpeed{
 		VERY_SLOW((byte)0),
 		SLOW((byte)1),
@@ -37,6 +50,10 @@ public class GeneEnums {
 			this.geneData = value;
 		}
 	}
+	
+	/*
+	 * Base health modification of the organism
+	 */
 	public enum health{
 		LARGE_REDUCTION((byte)0),
 		SMALL_REDUCTION((byte)1),
