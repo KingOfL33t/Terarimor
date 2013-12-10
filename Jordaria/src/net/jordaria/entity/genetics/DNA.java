95b94f6d55f8c33260cbdf004bbea592d595ea54
KingOfL33t <KingOfL33tGaming@gmail.com>
2013-12-09 21:49:29 -0600

    added getnormalgenes method

--- Jordaria/src/net/jordaria/entity/genetics/DNA.java
+++ Jordaria/src/net/jordaria/entity/genetics/DNA.java
@@ -1,5 +1,10 @@
 package net.jordaria.entity.genetics;
 
+import net.jordaria.entity.genetics.GeneEnums.genePosition;
+import net.jordaria.entity.genetics.GeneEnums.health;
+import net.jordaria.entity.genetics.GeneEnums.moveSpeed;
+import net.jordaria.entity.genetics.GeneEnums.size;
+
 public class DNA {
 	/*
 	 * This is the list of genes.
@@ -8,7 +13,14 @@ public class DNA {
 	private byte[] dna = new byte[32];
 	
 	public DNA(){
-		
+
 	}
 	
+	private byte[] getNormalGenes(){
+		byte[] geneArray = new byte[dna.length];
+		geneArray[genePosition.HEALTH.pos] = health.NORMAL.geneData;
+		geneArray[genePosition.MOVE_SPEED.pos] = moveSpeed.NORMAL.geneData;
+		geneArray[genePosition.SIZE.pos] = size.NORMAL.geneData;
+		return geneArray;
+	}
 }
