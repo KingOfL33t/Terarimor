1d39da1bfd3eeaea2acfda8ec3afcc080b2df2e7
KingOfL33t <KingOfL33tGaming@gmail.com>
2013-12-12 19:44:36 -0600

    Created colored text

---
+++ Jordaria/src/net/jordaria/text/ColoredText.java
@@ -0,0 +1,14 @@
+package net.jordaria.text;
+
+public class ColoredText {
+	String text;
+	Color color;
+	public ColoredText(String text){
+		this.text = text;
+		this.color = color.BLACK;
+	}
+	public ColoredText(String text, Color color){
+		this.text = text;
+		this.color = color;
+	}
+}
