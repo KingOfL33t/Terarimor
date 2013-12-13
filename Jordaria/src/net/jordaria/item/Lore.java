2a7a85fb9933e037c5fdcc6a68b08114adc3f57f
KingOfL33t <KingOfL33tGaming@gmail.com>
2013-12-12 19:42:39 -0600

    Added more lines to lore

--- Jordaria/src/net/jordaria/item/Lore.java
+++ Jordaria/src/net/jordaria/item/Lore.java
@@ -1,16 +1,52 @@
 package net.jordaria.item;
 
 import net.jordaria.text.Color;
+import net.jordaria.text.ColoredText;
 
 public class Lore {
-	public String lore;
-	public Color color;
+	public ColoredText line1;
+	public ColoredText line2;
+	public ColoredText line3;
 	
-	public Lore(String lore){
-		this.lore = lore;
+	public Lore(String line1){
+		this.line1 = new ColoredText(line1);
 	}
-	public Lore(String lore, Color loreColor){
-		this.lore = lore;
-		this.color = loreColor;
+	public Lore(String line1, Color line1color){
+		this.line1 = new ColoredText(line1, line1color);
+	}
+	public Lore(String line1, String line2){
+		this.line1 = new ColoredText(line1);
+		this.line2 = new ColoredText(line2);
+	}
+	public Lore(String line1, Color line1color, String line2, Color line2color){
+		this.line1 = new ColoredText(line1, line1color);
+		this.line2 = new ColoredText(line2, line2color);
+	}
+	public Lore(String line1, String line2, String line3){
+		this.line1 = new ColoredText(line1);
+		this.line2 = new ColoredText(line2);
+		this.line3 = new ColoredText(line3);
+	}
+	public Lore(String line1, Color line1color, String line2, Color line2color, String line3, Color line3color){
+		this.line1 = new ColoredText(line1, line1color);
+		this.line2 = new ColoredText(line2, line2color);
+		this.line3 = new ColoredText(line3, line3color);
+	}
+	public ColoredText getLine1(){
+		return line1;
+	}
+	public ColoredText getLine2(){
+		return line2;
+	}
+	public ColoredText getLine3(){
+		return line3;
+	}
+	public ColoredText getLine(int index){
+		switch(index){
+		case 1 : return getLine1();
+		case 2 : return getLine2();
+		case 3 : return getLine3();
+		default : return new ColoredText("");
+		}
 	}
 }
