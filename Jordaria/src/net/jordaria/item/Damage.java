25520f894416f3c0a0fe2ee4a688520e94ee7318
KingOfL33t <KingOfL33tGaming@gmail.com>
2013-12-12 19:44:15 -0600

    Added a damage class

---
+++ Jordaria/src/net/jordaria/item/Damage.java
@@ -0,0 +1,13 @@
+package net.jordaria.item;
+
+public class Damage {
+	public int base;
+	public int fire;
+	public int water;
+	public int stone;
+	public int poison;
+	public int ice;
+	public int magic;
+	public int holy;
+	public int dark;
+}
