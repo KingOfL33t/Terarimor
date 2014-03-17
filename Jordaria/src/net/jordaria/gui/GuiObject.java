dd95c617042b1a80095f38b4f7f5bb74a056c3eb
EISD <EISD@WH-RM276-S29.eaneside.local>
2014-03-17 10:29:55 -0500

    Updated GuiObject class

--- Jordaria/src/net/jordaria/gui/GuiObject.java
+++ Jordaria/src/net/jordaria/gui/GuiObject.java
@@ -1,6 +1,7 @@
 package net.jordaria.gui;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 import net.jordaria.event.EventManager;
 import net.jordaria.event.Listener;
@@ -16,10 +17,28 @@ public class GuiObject implements Listener{
 
 	private ArrayList<GuiObject> children;
 	private GuiObject parent;
+	private boolean hasParent = false;
 	private EventManager eventManager;
 	
 	/**
-	 * Preforms initialization of the gui object. 
+	 * Constructs a new {@link GuiObject}.
+	 */
+	public GuiObject(){
+		
+	}
+	
+	/**
+	 * Constructs a new {@link GuiObject} and sets the parent to the 
+	 * given object.
+	 * 
+	 * @param parent The parent object
+	 */
+	public GuiObject(GuiObject parent){
+		this();
+		setParent(parent);
+	}
+	/**
+	 * Performs initialization of the gui object. 
 	 * This should be overwritten.
 	 */
 	public void init(){
@@ -48,5 +67,61 @@ public class GuiObject implements Listener{
 		this.children.add(child);
 	}
 	
+	/**
+	 * Sets the parent reference to the provided object.
+	 * HasParent is set to true.
+	 * 
+	 * @param parent The new parent object
+	 */
+	public void setParent(GuiObject parent){
+		this.parent = parent;
+		this.hasParent = true;
+	}
+	
+	/**
+	 * Sets the parent reference to null and hasParent to false.
+	 */
+	public void removeParent(){
+		this.parent = null;
+		this.hasParent = false;
+	}
+	
+	/**
+	 * Recursively finds all of the children of this object and returns them 
+	 * in an {@link ArrayList}. getChildren() is called for the objects children as 
+	 * well.
+	 * 
+	 * @return all of this objects children
+	 */
+	public ArrayList<GuiObject> getChildren(){
+		LinkedList<GuiObject> open = new LinkedList<GuiObject>();
+		LinkedList<GuiObject> closed = new LinkedList<GuiObject>();
+		open.addAll(children);
+		while(!open.isEmpty()){
+			open.addAll(open.getFirst().getChildren());
+			closed.add(open.removeFirst());
+		}
+		ArrayList<GuiObject> output = new ArrayList<GuiObject>();
+		output.addAll(closed);
+		return output;
+	}
+	
+	/**
+	 * Returns true if this has a parent object, false otherwise.
+	 * 
+	 * @return true if this has a parent object, false otherwise.
+	 */
+	public boolean hasParent(){
+		return this.hasParent;
+	}
+	
+	/**
+	 * Returns the parent object. This can be null.
+	 * 
+	 * @return the parent object
+	 */
+	public GuiObject getParent(){
+		return this.parent;
+	}
 	
 }
